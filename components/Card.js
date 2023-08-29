c70c3de135316ac6e765924550b1d2a6a810aef9
Pavel Alekseev <pavelbooking.com@yandex.ru>
2023-08-29 21:44:28 +0500

    missed-cards, new features

--- components/Card.js
+++ components/Card.js
@@ -21,10 +21,10 @@ export default class Card {
         .content
         .querySelector('.basket__card')
         .cloneNode(true);
-  
-      this._quantity == 0 ? "" : this._element = cardElement;
+
+      this._element = cardElement;
     }
-  
+
     //Установка слушателей
     _setEventListeners() {
       this._likeButton.addEventListener('click', () => {
@@ -35,13 +35,13 @@ export default class Card {
         this._deleteCard();
       });
 
-      this._incrementButton.addEventListener('click', () => {
+      this._quantity > 0 ? this._incrementButton.addEventListener('click', () => {
         this._incrementQuantity();
-      });
+      }) : null;
 
-      this._decrementButton.addEventListener('click', () => {
+      this._quantity > 0 ? this._decrementButton.addEventListener('click', () => {
         this._decrementQuantity();
-      });
+      }) : null;
     }
   
     // кнопка like
@@ -52,12 +52,18 @@ export default class Card {
     // кнопка Удаления карточек
     _deleteCard() {
       initialCards = initialCards.filter( item  =>  item.id != this._id);
+      let result = initialCards.filter(card => card.quantity > 0);
+      let quantity = initialCards.filter(card => card.quantity == 0);
+      
       this._element.remove();
       this._element = null;
+      
+      document.querySelector ('.header__span-number').textContent = result.length;
+      quantity.length == 0 ? document.querySelector ('.basket__missed-items').textContent = " " : document.querySelector ('.basket__missed-items').textContent = "Отсутствуют · " + quantity.length + " товара";      
     }
 
     // активное и неактивное состояние у кнопок + и-
-    _togleButtonsCondition() {
+    _toggleButtonsCondition() {
       this._buyQuantity == this._quantity ? this._incrementButton.disabled = true : this._incrementButton.disabled = false;
       this._buyQuantity == 1 ? this._decrementButton.disabled = true : this._decrementButton.disabled = false;
     }
@@ -65,22 +71,22 @@ export default class Card {
     _incrementQuantity() {
       this._buyQuantity = this._buyQuantity + 1;
       this._cardQuantity.textContent = this._buyQuantity;
-      this._togleButtonsCondition();
+      this._toggleButtonsCondition();
     }
 
     _decrementQuantity() {
       this._buyQuantity = this._buyQuantity - 1;
       this._cardQuantity.textContent = this._buyQuantity;
-      this._togleButtonsCondition();
+      this._toggleButtonsCondition();
     }
   
     generateCard() {
       this._getTemplate();
-
+      
       this._likeButton = this._element.querySelector('.basket__like');
       this._deleteButton = this._element.querySelector('.basket__delete');
-      this._incrementButton = this._element.querySelector('.basket__increment');
-      this._decrementButton = this._element.querySelector('.basket__decrement');
+      this._incrementButton = this._element.querySelector('.basket__quantity-btn__increment');
+      this._decrementButton = this._element.querySelector('.basket__quantity-btn__decrement');
       this._cardImage = this._element.querySelector('.basket__image');
       this._cardHeader = this._element.querySelector('.basket__card-header');
       this._cardSize = this._element.querySelector('.basket__size');
@@ -90,19 +96,21 @@ export default class Card {
       this._cardQuantity = this._element.querySelector('.basket__buy-quantity');
       this._cardPrice = this._element.querySelector('.basket__price');
       this._cardFullPrice = this._element.querySelector('.basket__full-price');
+      this._cardMistake =  this._element.querySelector('.basket__mistake');
   
       this._cardImage.src = this._link;
       this._cardImage.alt = this._title;
       this._cardHeader.textContent = this._title;
-      this._size == "" ? this._cardSize === null: this._cardSize.textContent = "Размер:" + this._size;
-      this._color == "" ? this._cardColor === null: this._cardColor.textContent = "Цвет:" + this._color;
-      this._cardStorage.textContent = this._storage;
-      this._cardSeller.textContent = this._seller;
-      this._cardQuantity.textContent = this._buyQuantity;
-      this._cardPrice.textContent = this._price;
-      this._cardFullPrice.textContent = this._fullPrice;
+      this._size == "" ? this._cardSize === null : this._cardSize.textContent = "Размер:" + this._size;
+      this._color == "" ? this._cardColor === null : this._cardColor.textContent = "Цвет:" + this._color;
+      this._quantity > 0 ? this._cardStorage.textContent = this._storage : null;
+      this._quantity > 0 ? this._cardSeller.textContent = this._seller : null;
+      this._quantity > 0 ? this._cardQuantity.textContent = this._buyQuantity : null;
+      this._quantity > 0 ? this._cardPrice.textContent = this._price : null;
+      this._quantity > 0 ? this._cardFullPrice.textContent = this._fullPrice : null;
+      this._quantity > 0 ? ( this._quantity < 5 ? this._cardMistake.textContent = "Осталось " + this._quantity + " шт." : this._cardMistake === null ) : null; 
 
-     this. _togleButtonsCondition();
+      this._quantity > 0 ? this. _toggleButtonsCondition() : null;
 
       this._setEventListeners();
   
--- pages/index.js
+++ pages/index.js
@@ -2,32 +2,64 @@
 import Card from "../components/Card.js";
 import Section from "../components/Section.js";
 
-const cardList = '.basket__cards';
+const activeCardList = '.basket__cards';
+const inactiveCardList = '.basket__cards-missed';
 
-const createCard = (item) => {
-    const card = new Card(item, "#template");
+const createActiveCard = (item) => {
+  const activeCard = new Card(item, "#template");
     
-    const cardElement = card.generateCard();
+  const cardElement = activeCard.generateCard();
   
-    return cardElement;
-  };
+  return cardElement;
+};
+
+const createInactiveCard = (item) => {
+  const inactiveCard = new Card(item, "#template2");
+    
+  const cardElement = inactiveCard.generateCard();
   
-  const cards = new Section(
-    {
-      items: initialCards,
-      renderer: (item) => {
-        cards.addItem(createCard(item));
-      }
-    },
-    cardList
-  );
-  cards.renderItems();
+  return cardElement;
+};
+  
+const activeCards = new Section(
+  {
+    items: initialCards.filter(card => card.quantity > 0),
+    renderer: (item) => {
+      activeCards.addItem(createActiveCard(item));
+    }
+  },
+  activeCardList
+);
+
+const inactiveCards = new Section(
+  {
+    items: initialCards.filter(card => card.quantity == 0),
+    renderer: (item) => {
+      inactiveCards.addItem(createInactiveCard(item));
+    }
+  },
+  inactiveCardList
+);
+
+activeCards.renderItems();
+inactiveCards.renderItems();
 
 // Вставка даанных в хэдер корзины(колличкство товаров)
+
 let result = initialCards.filter(card => card.quantity > 0);
 
 function renderSpanNumber(result) {
-    document.querySelector ('.header__span-number').textContent = result.length
+  document.querySelector ('.header__span-number').textContent = result.length
 }
 
 renderSpanNumber(result);
+
+//Вставка колличества отсутствующих товаров
+
+let quantity = initialCards.filter(card => card.quantity == 0);
+
+function renderMissedItemNumber(quantity) {
+  document.querySelector ('.basket__missed-items').textContent = "Отсутствуют · " + quantity.length + " товара"
+}
+
+renderMissedItemNumber(quantity);
